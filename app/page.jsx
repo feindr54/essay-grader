4f9d7cdea98b6d40da9fafcb3f6dcf81c5938188
Ronald Tsai <tsai210@purdue.edu>
2024-03-30 18:03:44 -0400

    Added titles

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,13 @@
 const Home = () => {
   return (
-    <div>Home</div>
-  ) 
+    <section className = "w-full flex-center flex-col">
+        <h1 className = "head_text text-center">
+            AI-Powered Essay Grading
+            <br className = "max-md:hidden" />
+            <span className = "green_gradient text-center">Human-Centric Learning</span>
+        </h1>
+    </section>
+  )
 }
 
 export default Home
