2b4d17587426e11ecefa9b27522357bb8a128c99
feindr54 <cxgao1729@gmail.com>
2024-03-31 06:09:48 -0400

    added ai support

--- api/app.py
+++ api/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS 
+from flask_cors import CORS
 from model.model import evaluate, load_model
 
 app = Flask(__name__)
 CORS(app)
+
+model = load_model("model/model.tp")
 
 @app.route("/api/python/", methods=["GET"])
 def hello_world():
@@ -11,9 +13,11 @@ def hello_world():
 
 @app.route("/api/request/", methods=["GET", "POST"])
 def send_msg():
-    return jsonify({'message': 'I have replied'})
+    content = request.get_json()["postContent"]
+    score = evaluate(model, content)
+    return jsonify({'message': score})
 
- 
+
 
 if __name__ == '__main__':
     app.run(debug=True)
